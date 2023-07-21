586da9a9a8503bbac4cc5af9f31640add98ebcb0
mansoorabbas123 <mansoor.abbas651@gmail.com>
2023-07-21 17:03:39 -0400

    sanity setup

---
+++ sanity/products.ts
@@ -0,0 +1,12 @@
+export const products = {
+    name: 'products',
+    type: 'document',
+    title: 'Product',
+    fields: [
+        {
+            name: 'title',
+            type: 'string',
+            title: 'Name',
+        }
+    ]
+}
